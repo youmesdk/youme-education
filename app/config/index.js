4b292746f55a2ce780d27031b32dd6fe0b07e49a
李繁 <lifan@youme.im>
2019-01-20 21:41:37 +0800

    屏蔽区域参数，只保留中国和香港

--- app/config/index.js
+++ app/config/index.js
@@ -15,19 +15,19 @@ export const VIDEO_REGION_NAME = 'RTC_CN_SERVER'; // 服务器区域地址
 export const REGION_MAP = [
   { code: 0, name: 'RTC_CN_SERVER', label: 'China' },   // 中国
   { code: 1, name: 'RTC_HK_SERVER', label: 'Hong Kong' },  // 香港
-  { code: 2, name: 'RTC_US_SERVER', label: 'U.S.A' },  // 美国
-  { code: 3, name: 'RTC_SG_SERVER', label: 'Singapore' },  // 新加坡
-  { code: 4, name: 'RTC_KR_SERVER', label: 'Korea' },  // 韩国
-  { code: 5, name: 'RTC_AU_SERVER', label: 'Australia' }, // 澳大利亚
-  { code: 6, name: 'RTC_DE_SERVER', label: 'Germany' },  // 德国
-  { code: 7, name: 'RTC_BR_SERVER', label: 'Brazil' },  // 巴西
-  { code: 8, name: 'RTC_IN_SERVER', label: 'India' },  // 印度
-  { code: 9, name: 'RTC_JP_SERVER', label: 'Japan' },  // 日本
-  { code: 10, name: 'RTC_IE_SERVER', label: 'Ireland' },  // 爱尔兰
-  { code: 11,  name: 'RTC_USW_SERVER', label: 'Western United States' }, // 美国西部
-  { code: 12, name: 'RTC_USM_SERVER', label: 'Central United States' }, // 美国中部
-  { code: 13,  name: 'RTC_CA_SERVER',  label: 'Canada' }, // 加拿大
-  { code: 14, name: 'RTC_LON_SERVER', label: 'London' }, // 伦敦
-  { code: 15, name: 'RTC_FRA_SERVER', label: 'Frankfurt' }, // 法兰克福
-  { code: 16, name: 'RTC_DXB_SERVER', label: 'Dubai' }, // 迪拜
+  // { code: 2, name: 'RTC_US_SERVER', label: 'U.S.A' },  // 美国
+  // { code: 3, name: 'RTC_SG_SERVER', label: 'Singapore' },  // 新加坡
+  // { code: 4, name: 'RTC_KR_SERVER', label: 'Korea' },  // 韩国
+  // { code: 5, name: 'RTC_AU_SERVER', label: 'Australia' }, // 澳大利亚
+  // { code: 6, name: 'RTC_DE_SERVER', label: 'Germany' },  // 德国
+  // { code: 7, name: 'RTC_BR_SERVER', label: 'Brazil' },  // 巴西
+  // { code: 8, name: 'RTC_IN_SERVER', label: 'India' },  // 印度
+  // { code: 9, name: 'RTC_JP_SERVER', label: 'Japan' },  // 日本
+  // { code: 10, name: 'RTC_IE_SERVER', label: 'Ireland' },  // 爱尔兰
+  // { code: 11,  name: 'RTC_USW_SERVER', label: 'Western United States' }, // 美国西部
+  // { code: 12, name: 'RTC_USM_SERVER', label: 'Central United States' }, // 美国中部
+  // { code: 13,  name: 'RTC_CA_SERVER',  label: 'Canada' }, // 加拿大
+  // { code: 14, name: 'RTC_LON_SERVER', label: 'London' }, // 伦敦
+  // { code: 15, name: 'RTC_FRA_SERVER', label: 'Frankfurt' }, // 法兰克福
+  // { code: 16, name: 'RTC_DXB_SERVER', label: 'Dubai' }, // 迪拜
 ];
